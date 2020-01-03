90f449e267aaf7cce837a7ef3acf1a632e710816
wcgmiracle@gmail.com <wcg47230554>
2020-01-03 22:19:59 +0800

    react security config

--- springboot-spring-security-core/src/main/java/com/springboot/security/config/security/config/UserPrincipal.java
+++ springboot-spring-security-core/src/main/java/com/springboot/security/config/security/config/UserPrincipal.java
@@ -38,7 +38,7 @@ public class UserPrincipal implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
+        return authorities;
     }
 
     @Override
